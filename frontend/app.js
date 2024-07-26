3e61740d940ca49d466e47ee4a466842e4ea948e
Anouar <anouarelmaaroufi@gmail.com>
2024-07-27 00:24:33 +0100

    Building the initial chat page template frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,5 @@
 import { NavBar } from "./js/components/Navbar.js";
+import { ChatPage } from "./js/pages/ChatPage.js";
 import { GamePage } from "./js/pages/GamePage.js";
 import { HomePage } from "./js/pages/HomePage.js";
 import { LoginPage } from "./js/pages/LoginPage.js";
@@ -12,4 +13,19 @@ app.isLoggedIn = true;
 
 window.addEventListener("DOMContentLoaded", () => {
   app.router.init();
+  sendMessageActionStyling();
 });
+
+// This is temporary for me Anouar, later will be placed in a custom HTML Element 😁
+function sendMessageActionStyling() {
+  const input = document.querySelector(".send_message_container input");
+  const container = document.querySelector(".send_message_container");
+
+  input.addEventListener("focus", () => {
+    container.classList.add("focused");
+  });
+
+  input.addEventListener("blur", () => {
+    container.classList.remove("focused");
+  });
+}
