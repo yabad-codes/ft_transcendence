e06993959a6265180c740a402a81231df60aa2d4
Yabad <103969464+yabad-codes@users.noreply.github.com>
2024-07-29 07:46:22 +0100

    Update views.py

--- backend/pong_service/apps/authentication/views.py
+++ backend/pong_service/apps/authentication/views.py
@@ -1,13 +1,11 @@
 from rest_framework.generics import ListCreateAPIView, ListAPIView
 from .serializers import *
 from .models import Player
-from rest_framework.permissions import AllowAny
-from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from django.contrib.auth import login, logout
-from rest_framework.permissions import IsAuthenticated
 
 
 class LogoutView(APIView):
