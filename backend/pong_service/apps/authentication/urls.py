2bad8862146d7649ab002fece9b8b4eaa81db5a0
Yahya Abad <yabad@c1r2p9.1337.ma>
2024-07-31 11:07:24 +0100

    adding config to facilitate debuging backend apps

--- backend/pong_service/apps/authentication/urls.py
+++ backend/pong_service/apps/authentication/urls.py
@@ -1,11 +1,11 @@
-from .views import *
+import pong_service.apps.authentication.views as views
 from django.urls import path
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('players/', PlayerListView.as_view(), name='player_list'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('players/', views.PlayerListView.as_view(), name='player_list'),
     path('profile/<str:username>/',
-         PlayerPublicProfileView.as_view(), name='player_profile'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+         views.PlayerPublicProfileView.as_view(), name='player_profile'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
