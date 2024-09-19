54ae03ff540e5177426ef403982e6013235f0637
Yahya Abad <yabad@c1r7p4.1337.ma>
2024-09-19 17:43:21 +0100

    adding helper functions that connects and gets information from 42 API

--- backend/pong_service/apps/authentication/helpers.py
+++ backend/pong_service/apps/authentication/helpers.py
@@ -1,5 +1,12 @@
+import random
 import bleach
+import logging
+import requests
+
+from django.conf import settings
+
 from rest_framework import serializers
+
 from .models import Player
 from django.conf import settings
 
@@ -76,6 +83,7 @@ def create_player(validated_data):
 
 	return Player.objects.create_user(
 		username=validated_data['username'],
+		api_user_id=validated_data.get('api_user_id', None),
 		first_name=validated_data['first_name'],
 		last_name=validated_data['last_name'],
 		password=validated_data['password'],
@@ -122,3 +130,56 @@ def set_cookie(response, key, value, max_age):
         path=settings.AUTH_COOKIE_PATH
     )
     return response
+
+def get_42_token(code):
+    token_url = 'https://api.intra.42.fr/oauth/token'
+    token_data = {
+		'grant_type': 'authorization_code',
+		'code': code,
+		'client_id': settings.UID,
+		'client_secret': settings.SECRET,
+		'redirect_uri': settings.REDIRECT_URL
+	}
+    token_response = requests.post(token_url, data=token_data)
+    return token_response.json()
+
+def get_42_user_data(access_token):
+    api_url = 'https://api.intra.42.fr/v2/me'
+    headers = {
+		'Authorization': f'Bearer {access_token}'
+	}
+    
+    response = requests.get(api_url, headers=headers)
+    return response.json()
+
+def construct_user_data(user_data):
+    data = {}
+    
+    data['username'] = get_unique_username(user_data['login'], user_data['id'])
+    data['api_user_id'] = user_data['id']
+    data['first_name'] = user_data['first_name']
+    data['last_name'] = user_data['last_name']
+    data['password'] = None
+    data['password_confirm'] = None
+    data['avatar'] = user_data['image']['link']
+    
+    return data
+
+def get_unique_username(username, api_user_id):
+	username_exists = Player.objects.filter(username=username).exists()
+	api_user_id_exists = Player.objects.filter(api_user_id=api_user_id).exists()
+	
+	if not api_user_id_exists and not username_exists:
+		return username
+	
+	if not api_user_id_exists and username_exists:
+		while Player.objects.filter(username=username).exists():
+			username = f'{username}{random.randint(0, 100)}'
+		return username
+
+	if api_user_id_exists:
+		player = Player.objects.get(api_user_id=api_user_id)
+		return player.username
+
+def user_already_exists(user_data):
+    return Player.objects.filter(api_user_id=user_data['api_user_id']).exists()
