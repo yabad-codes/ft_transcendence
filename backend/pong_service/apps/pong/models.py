18dde6bfb38725c4255955626b79075afd81c9e5
Yahya Abad <yabad@c1r8p1.1337.ma>
2024-09-22 20:59:33 +0100

    updating winner field to be blank or null

--- backend/pong_service/apps/pong/models.py
+++ backend/pong_service/apps/pong/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from pong_service.apps.authentication.models import Player
 
 class PongGame(models.Model):
-    STATUS = [
-		('pending', 'Game Pending'),
-		('started', 'Game Started'),
-		('finished', 'Game Finished'),
-	]
-    id = models.AutoField(primary_key=True)
+    class Status(models.TextChoices):
+        PENDING = 'Pending'
+        STARTED = 'Started'
+        FINISHED = 'Finished'
+
+    id = models.AutoField(primary_key=True, editable=False)
     player1 = models.ForeignKey(Player, related_name='player1', on_delete=models.CASCADE)
     player2 = models.ForeignKey(Player, related_name='player2', on_delete=models.CASCADE)
-    winner = models.ForeignKey(Player, related_name='winner', on_delete=models.CASCADE)
+    winner = models.ForeignKey(Player, related_name='winner', on_delete=models.SET_NULL, null=True, blank=True)
     player1_score = models.PositiveIntegerField(default=0)
     player2_score = models.PositiveIntegerField(default=0)
-    status = models.CharField(max_length=20, choices=STATUS, default='pending')
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
