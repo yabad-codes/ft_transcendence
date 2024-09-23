a9daabc7527010bcbfef10ca4a30077d69af5e10
Yahya Abad <yabad@c1r7p4.1337.ma>
2024-09-23 12:48:43 +0100

    adding routings for matchmaking websocket

--- backend/pong_service/apps/pong/routing.py
+++ backend/pong_service/apps/pong/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from . import consumers
+from pong_service.apps.pong.consumers import PongConsumer, MatchMakingConsumer
 
 websocket_urlpatterns = [
-	re_path(r'ws/pong/$', consumers.PongConsumer.as_asgi()),
+	re_path(r'ws/pong/$', PongConsumer.as_asgi()),
+	re_path(r'ws/matchmaking/$', MatchMakingConsumer.as_asgi()),
 ]
