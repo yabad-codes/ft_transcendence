9412410d3a365ab4af3f231143be02301b42c4f3
Yahya Abad <yabad@c1r4p10.1337.ma>
2024-09-28 23:16:11 +0100

    .

--- frontend/app.js
+++ frontend/app.js
@@ -7,14 +7,14 @@ import { LoginPage } from "./js/pages/LoginPage.js";
 import { RegisterPage } from "./js/pages/RegisterPage.js";
 import { ChatMessage } from "./js/components/ChatMessage.js";
 import { LeaderboardPage } from "./js/pages/LeaderboardPage.js";
-import API from "./js/utils/API.js";
+import api from "./js/utils/api.js";
 import Router from "./js/utils/router.js";
 
 // global app object that is available on all JS source code, later will be modified as our needs
 window.app = {};
 
 app.router = Router;
-app.api = API;
+app.api = api;
 app.isLoggedIn = true;
 app.profile = null;
 
