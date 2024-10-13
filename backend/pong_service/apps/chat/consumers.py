a227e3554c3c16fd79cddcd3bc861f669d3ca52b
Yahya Abad <yabad@c1r5p8.1337.ma>
2024-10-13 15:33:38 +0100

    when the player disconnect from notification center, we check for pending game requests to remove them

--- backend/pong_service/apps/chat/consumers.py
+++ backend/pong_service/apps/chat/consumers.py
@@ -6,154 +6,164 @@ from pong_service.helpers import get_user_from_access_token
 from django.db.models import Q
 from django.conf import settings
 
+
 class ChatConsumer(AsyncWebsocketConsumer):
-	async def connect(self):
-		self.cookies = self.scope['cookies']
-		self.access_token = self.cookies.get(settings.AUTH_COOKIE)
-		self.user = await get_user_from_access_token(self.access_token)
+    async def connect(self):
+        self.cookies = self.scope['cookies']
+        self.access_token = self.cookies.get(settings.AUTH_COOKIE)
+        self.user = await get_user_from_access_token(self.access_token)
 
-		if self.user is None:
-			await self.close()
-			return None
+        if self.user is None:
+            await self.close()
+            return None
 
-		self.room_name = f'chat_{self.user.id}'
-		await self.channel_layer.group_add(
-			self.room_name,
-			self.channel_name
-		)
-		await self.accept()
+        self.room_name = f'chat_{self.user.id}'
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+        await self.accept()
 
-	async def disconnect(self, close_code):
-		if self.user is None:
-			return None
+    async def disconnect(self, close_code):
+        if self.user is None:
+            return None
 
-		await self.channel_layer.group_discard(
-			self.room_name,
-			self.channel_name
-		)
-	
-	async def receive(self, text_data):
-		pass
-	
-	async def chat_message(self, event):
-		message = event['message']
-		await self.send(text_data=json.dumps({
-			'message': message
-		}))
+        await self.channel_layer.group_discard(
+            self.room_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        pass
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
 
 def send_message(user_id, conversationID, message):
-	channel_layer = get_channel_layer()
+    channel_layer = get_channel_layer()
 
-	if not channel_layer:
+    if not channel_layer:
         # Log error or raise an exception as needed
-		print("Channel layer is not available.")
-		return
+        print("Channel layer is not available.")
+        return
 
-	try:
-		async_to_sync(channel_layer.group_send)(
-			f'chat_{user_id}',
-			{
-				'type': 'chat_message',
-				'message': {
-					'conversation_id': conversationID,
-					'data': message
-				}
-			}
-		)
-	except Exception as e:
-		# Handle exceptions, possibly logging or retrying
-		print(f"Failed to send message via WebSocket: {e}")
+    try:
+        async_to_sync(channel_layer.group_send)(
+            f'chat_{user_id}',
+            {
+                'type': 'chat_message',
+                'message': {
+                    'conversation_id': conversationID,
+                    'data': message
+                }
+            }
+        )
+    except Exception as e:
+        # Handle exceptions, possibly logging or retrying
+        print(f"Failed to send message via WebSocket: {e}")
+
 
 class NotificationConsumer(AsyncWebsocketConsumer):
-	async def connect(self):
-		# settings.configure()
+    async def connect(self):
+        # settings.configure()
 
-		self.cookies = self.scope['cookies']
-		self.access_token = self.cookies.get(settings.AUTH_COOKIE)
-		self.user = await get_user_from_access_token(self.access_token)
+        self.cookies = self.scope['cookies']
+        self.access_token = self.cookies.get(settings.AUTH_COOKIE)
+        self.user = await get_user_from_access_token(self.access_token)
 
-		if self.user is None:
-			await self.close()
-			return None
+        if self.user is None:
+            await self.close()
+            return None
 
-		self.room_name = f'notification_{self.user.id}'
-		await self.channel_layer.group_add(
-			self.room_name,
-			self.channel_name
-		)
-		await self.accept()
+        self.room_name = f'notification_{self.user.id}'
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+        await self.accept()
 
-		self.user.online = True
-		await sync_to_async(self.user.save)()
-		await self.sendOnlineStatusToFriends()
+        self.user.online = True
+        await sync_to_async(self.user.save)()
+        await self.sendOnlineStatusToFriends()
 
-	async def disconnect(self, close_code):
-		from pong_service.apps.authentication.models import Player
-		if self.user is None:
-			return None
+    async def disconnect(self, close_code):
+        from pong_service.apps.authentication.models import Player
+        from pong_service.apps.pong.models import GameRequest
 
-		player = await sync_to_async(Player.objects.get)(id=self.user.id)
-		player.online = False
-		self.user.online = False
-		await sync_to_async(player.save)()
-		await self.sendOnlineStatusToFriends()
+        if self.user is None:
+            return None
 
-		await self.channel_layer.group_discard(
-			self.room_name,
-			self.channel_name
-		)
+        # Check if the user in a game request and remove it
+        game_request = await sync_to_async(GameRequest.objects.filter)(Q(player=self.user) | Q(opponent=self.user))
+        if game_request.exists():
+            await sync_to_async(game_request.delete)()
 
-	async def receive(self, text_data):
-		pass
+        player = await sync_to_async(Player.objects.get)(id=self.user.id)
+        player.online = False
+        self.user.online = False
+        await sync_to_async(player.save)()
+        await self.sendOnlineStatusToFriends()
 
-	async def notification_message(self, event):
-		message = event['message']
-		await self.send(text_data=json.dumps({
-			'message': message
-		}))
+        await self.channel_layer.group_discard(
+            self.room_name,
+            self.channel_name
+        )
 
-	async def sendOnlineStatusToFriends(self):
-		from pong_service.apps.chat.models import Friendship
+    async def receive(self, text_data):
+        pass
 
-		friends = await sync_to_async(list)(Friendship.objects.filter(
-			Q(player1=self.user, friendshipAccepted=True) | 
-			Q(player2=self.user, friendshipAccepted=True)
-		).values_list('player1_id', 'player2_id'))
+    async def notification_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
 
-		for friend in friends:
-			friend_id = friend[1] if friend[0] == self.user.id else friend[0]
-			await self.channel_layer.group_send(
-				f'notification_{friend_id}',
-				{
-					'type': 'notification_message',
-					'message': {
-						'type': 'online_status',
-						'username': self.user.username,
-						'online': self.user.online
-					}
-				}
-			)
+    async def sendOnlineStatusToFriends(self):
+        from pong_service.apps.chat.models import Friendship
 
-	@staticmethod
-	def sendFriendRequestNotification(user_id, friend_id):
-		channel_layer = get_channel_layer()
+        friends = await sync_to_async(list)(Friendship.objects.filter(
+            Q(player1=self.user, friendshipAccepted=True) |
+            Q(player2=self.user, friendshipAccepted=True)
+        ).values_list('player1_id', 'player2_id'))
 
-		if not channel_layer:
-			# Log error or raise an exception as needed
-			print("Channel layer is not available.")
-			return
+        for friend in friends:
+            friend_id = friend[1] if friend[0] == self.user.id else friend[0]
+            await self.channel_layer.group_send(
+                f'notification_{friend_id}',
+                {
+                    'type': 'notification_message',
+                    'message': {
+                        'type': 'online_status',
+                        'username': self.user.username,
+                        'online': self.user.online
+                    }
+                }
+            )
 
-		try:
-			async_to_sync(channel_layer.group_send)(
-				f'notification_{friend_id}',
-				{
-					'type': 'notification_message',
-					'message': {
-						'type': 'friend_request',
-						'user_id': user_id
-					}
-				}
-			)
-		except Exception as e:
-			# Handle exceptions, possibly logging or retrying
-			print(f"Failed to send message via WebSocket: {e}")
+    @staticmethod
+    def sendFriendRequestNotification(user_id, friend_id):
+        channel_layer = get_channel_layer()
+
+        if not channel_layer:
+            # Log error or raise an exception as needed
+            print("Channel layer is not available.")
+            return
+
+        try:
+            async_to_sync(channel_layer.group_send)(
+                f'notification_{friend_id}',
+                {
+                    'type': 'notification_message',
+                    'message': {
+                        'type': 'friend_request',
+                        'user_id': user_id
+                    }
+                }
+            )
+        except Exception as e:
+            # Handle exceptions, possibly logging or retrying
+            print(f"Failed to send message via WebSocket: {e}")
